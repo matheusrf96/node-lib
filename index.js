c2e497e30f7a045fbc5f93cc7ffe08af05b744ed
Matheus Rodrigues <matheusrf96@gmail.com>
2022-10-05 16:39:54 -0300

    feat (index): Create getFile function using fs lib

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('olá mundo!'));
+function getFile(filePath) {
+    const encoding = 'utf-8';
+    fs.readFile(filePath, encoding, (_, text) => {
+        console.log(chalk.green(text));
+    });
+}
+
+getFile('./arquivos/texto.md');
