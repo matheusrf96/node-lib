91e6fddb600af3c593613502581105521d1e194e
Matheus Rodrigues <matheusrf96@gmail.com>
2022-10-05 16:52:30 -0300

    refactor (index): Handle error on function getFile

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function handlerError(err) {
+    throw new Error(chalk.red(err.code, 'Não há arquivo no diretório'));
+}
+
 function getFile(filePath) {
     const encoding = 'utf-8';
-    fs.readFile(filePath, encoding, (_, text) => {
+    fs.readFile(filePath, encoding, (err, text) => {
+        if (err) {
+            handlerError(err)
+        }
+
         console.log(chalk.green(text));
     });
 }
