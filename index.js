4d3e26ec10e3c555ac7f03aa48e4e7844081d3fb
Matheus Rodrigues <matheusrf96@gmail.com>
2022-10-05 17:19:03 -0300

    refactor (index): Replace then with async/await

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ function handleError(err) {
     throw new Error(chalk.red(err.code, 'Não há arquivo no diretório'));
 }
 
-function getFile(filePath) {
+async function getFile(filePath) {
     const encoding = 'utf-8';
 
-    fs.promises.readFile(filePath, encoding)
-        .then((text) => console.log(chalk.green(text)))
-        .catch((err) => handleError(err))
+    try {
+        const text = await fs.promises.readFile(filePath, encoding)
+        console.log(chalk.green(text));
+    } catch (err) {
+        handleError(err);
+    }
 }
 
 getFile('./arquivos/texto.md');
