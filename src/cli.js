b496816b8edfbb21f727f44e454008a635a5c770
Matheus Rodrigues <matheusrf96@gmail.com>
2022-10-06 14:54:01 -0300

    refactor (cli): Handle errors and improve data display

--- src/cli.js
+++ src/cli.js
@@ -5,21 +5,30 @@ import getFile from "./index.js";
 
 const args = process.argv;
 
-function printList(result) {
-    console.log(chalk.yellow('lista de links: '), result);
+function printList(result, filename = '') {
+    console.log(chalk.yellow('lista de links: '), chalk.black.bgGreen(filename), result);
 }
 
 async function processText(args) {
     const path = args[2];
+
+    try {
+        fs.lstatSync(path);
+    } catch(err) {
+        if (err.code === 'ENOENT') {
+            console.log(chalk.red('arquivo ou diretório não existe'));
+            return;
+        }
+    }
 
     if (fs.lstatSync(path).isFile()) {
         const result = await getFile(path);
         printList(result);
     } else if (fs.lstatSync(path).isDirectory()) {
         const files = await fs.promises.readdir(path);
-        files.forEach(async (filepath) => {
-            const list = await getFile(`${path}/${filepath}`);
-            printList(list);
+        files.forEach(async (filename) => {
+            const list = await getFile(`${path}/${filename}`);
+            printList(list, filename);
         })
     }
 }
