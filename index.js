92353153b79d986b307a504fc69b2744d353ade6
Matheus Rodrigues <matheusrf96@gmail.com>
2022-10-06 14:03:51 -0300

    feat (index): Implement regex to extract links from text

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function extractLinks(text) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captures = [...text.matchAll(regex)];
+    const results = captures.map(capture => ({[capture[1]]: capture[2]}));
+
+    return results;
+}
+
 function handleError(err) {
     throw new Error(chalk.red(err.code, 'Não há arquivo no diretório'));
 }
@@ -10,7 +18,7 @@ async function getFile(filePath) {
 
     try {
         const text = await fs.promises.readFile(filePath, encoding)
-        console.log(chalk.green(text));
+        console.log(extractLinks(text));
     } catch (err) {
         handleError(err);
     }
