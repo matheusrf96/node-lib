8a3d81c993bcd82ba93c6e136cca188121c50167
Matheus Rodrigues <matheusrf96@gmail.com>
2022-10-06 14:45:28 -0300

    feat (cli): Adapt script to process both files and directories

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,27 @@
+import fs from 'fs';
 import chalk from "chalk";
 
 import getFile from "./index.js";
 
-const path = process.argv;
+const args = process.argv;
 
-async function processText(path) {
-    const result = await getFile(path[2]);
-    console.log(chalk.yellow('Lista de links: '), result)
+function printList(result) {
+    console.log(chalk.yellow('lista de links: '), result);
 }
 
-processText(path)
+async function processText(args) {
+    const path = args[2];
+
+    if (fs.lstatSync(path).isFile()) {
+        const result = await getFile(path);
+        printList(result);
+    } else if (fs.lstatSync(path).isDirectory()) {
+        const files = await fs.promises.readdir(path);
+        files.forEach(async (filepath) => {
+            const list = await getFile(`${path}/${filepath}`);
+            printList(list);
+        })
+    }
+}
+
+processText(args)
